12baeb8b5cd4371b3158d321c29a2d06f58d465f
koroket <bmasakazu@gmail.com>
2014-11-23 10:31:12 -0800

    Initial Commit

---
+++ WatchKitHackathon2014/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WatchKitHackathon2014
+//
+//  Created by sloot on 11/23/14.
+//  Copyright (c) 2014 sloot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
